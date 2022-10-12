ab3a3e1d32b7266d733f24d5a1c289f4e089647c
Road2BigMoney <usaakk99@gmail.com>
2022-10-12 03:39:12 +0300

    first changes

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -12,7 +12,6 @@ public class MainApp {
    public static void main(String[] args) throws SQLException {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
-
       UserService userService = context.getBean(UserService.class);
 
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -18,6 +19,10 @@ public class User {
 
    @Column(name = "email")
    private String email;
+
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "cars_id")
+   private Car empCar;
 
    public User() {}
    
@@ -57,5 +62,37 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getEmpCar() {
+      return empCar;
+   }
+
+   public void setEmpCar(Car empCar) {
+      this.empCar = empCar;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof User)) return false;
+      User user = (User) o;
+      return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email) && Objects.equals(empCar, user.empCar);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(id, firstName, lastName, email, empCar);
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", empCar=" + empCar +
+              '}';
    }
 }
