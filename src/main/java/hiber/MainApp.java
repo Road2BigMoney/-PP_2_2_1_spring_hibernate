2e9e0ec970b1e7d3c084e62b657b0ccbc12e725f
Road2BigMoney <usaakk99@gmail.com>
2022-10-13 22:53:44 +0300

    added dao and service to Car

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -3,6 +3,7 @@ package hiber;
 import hiber.config.AppConfig;
 import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,13 +15,34 @@ public class MainApp {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
       UserService userService = context.getBean(UserService.class);
+      CarService carService = context.getBean(CarService.class);
+
 
 //      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
 //      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
 //      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
 //      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("BMW", 7)));
-      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("VAZ", 21140)));
+//      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("BMW", 7)));
+//      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("VAZ", 21140)));
+      User[] userArray = new User[3];
+      userArray[0] = new User("Jacque ","Fresco","afterLifeMailBox@gmail.com");
+      userArray[1] = new User("Dr", "Livesey", "dr.livesey@gmail.com");
+      userArray[2] = new User("Hasbulla", "Magomedov","theBiggest@gmail.com");
+
+      Car car1 = new Car("BMW", 7);
+      Car car2 = new Car("VAZ", 21140);
+      Car car3 = new Car("Mercedes-Benz S-class", 223);
+
+      carService.add(car1);
+      carService.add(car2);
+      carService.add(car3);
+      int i = 0;
+      for (Car car : carService.listCars()) {
+         userArray[i].setCar(car);
+         userService.add(userArray[i++]);
+      }
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -31,7 +53,9 @@ public class MainApp {
          System.out.println("Car = " + user.getCar());
          System.out.println();
       }
+      System.out.println();
       System.out.println(userService.getUserByCar("VAZ", 21140));
+      System.out.println();
 
 
       context.close();
